c3a7b0b74861dda8d598640da0ef7a81e3ec90f7
JitlerJudio <est.juan.djejen@unimilitar.edu.co>
2024-08-31 16:29:49 -0500

    segundo entrega de la correción de parcial
    Se adjunta la nueva clase polígono y sus respectivos métodos, como lo son CalcularRadio y Dibujar

--- CORTE2/parcial1-ok.js
+++ CORTE2/parcial1-ok.js
@@ -30,3 +30,40 @@ class Polar {
     }
 }
 
+class Poligono {
+    #n;
+    #centroX;
+    #centroY;
+    #medida;
+    #tipoMedida;
+
+    constructor(n, centroX, centroY, medida, tipoMedida) {
+        this.#n = n;
+        this.#centroX = centroX;
+        this.#centroY = centroY;
+        this.#medida = medida;
+        this.#tipoMedida = tipoMedida;
+    }
+
+    calcularRadio() {
+        if (this.#tipoMedida === 'apotema') {
+            return this.#medida / Math.cos(Math.PI / this.#n);
+        } else {
+            return this.#medida / (2 * Math.sin(Math.PI / this.#n));
+        }
+    }
+
+    dibujar(ctx) {
+        const radio = this.calcularRadio();
+        ctx.beginPath();
+        for (let i = 0; i < this.#n; i++) {
+            const angulo = (i * 2 * Math.PI) / this.#n - Math.PI / 2;
+            const x = this.#centroX + radio * Math.cos(angulo);
+            const y = this.#centroY + radio * Math.sin(angulo);
+            ctx.lineTo(x, y);
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
